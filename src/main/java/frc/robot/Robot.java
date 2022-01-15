d59154430c4557e78b9a371427dbeadb76c24773
Aadit-Seagull <aadit.sahgal@gmail.com>
2022-01-15 15:03:58 -0600

    Created a shooterPID

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -11,6 +11,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.commands.auto.AethiaLeftThreeCells;
+import frc.robot.commands.auto.shooterPID;
 import frc.robot.subsystems.DriveTrain;
 
 
@@ -103,6 +104,8 @@ public class Robot extends TimedRobot
   @Override
   public void autonomousPeriodic() {
     CommandScheduler.getInstance().run();
+    RobotContainer.getShooter().getShooterMotorTop().set(shooterPID.getShooterPID().calculate(RobotContainer.getShooter().getTopEnc().getDistance()));
+
   }
 
   @Override
